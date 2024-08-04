8b6bf9dc463cbbc5d73cf4b0970e80d3d3a3bec3
alstrokov <strokov.aleksandr@gmail.com>
2024-08-04 22:55:10 +0300

    Add project files.

---
+++ PingConMonitor/DataStructures/LastTimes.cs
@@ -0,0 +1,76 @@
+﻿namespace PingConMonitor.DataStructures
+{
+    internal class LastTimes
+    {
+        private Dictionary<long, DateTime> lastTimes = new();
+
+        public void Update(long duration)
+        {
+
+            if (duration == 0)
+            {
+                lastTimes[0] = DateTime.Now;
+            }
+
+            foreach (var key in lastTimes.Keys)
+            {
+                if (key == 0)
+                    continue;
+
+                if (duration >= key)
+                {
+                    lastTimes[key] = DateTime.Now;
+                }
+            }
+        }
+
+        public LastTimes()
+        {
+            lastTimes = new Dictionary<long, DateTime>()
+            {
+                { 0, DateTime.Now },
+                { 25, DateTime.Now },
+                { 50, DateTime.Now },
+                { 75, DateTime.Now },
+                { 100, DateTime.Now },
+                { 200, DateTime.Now },
+                { 500, DateTime.Now },
+                { 1000, DateTime.Now },
+                { 1500, DateTime.Now },
+                { 2000, DateTime.Now},
+                { 3000, DateTime.Now }
+            };
+        }
+
+        public void ShowAll()
+        {
+            double totalSeconds;
+
+            foreach (var key in lastTimes.Keys)
+            {
+                totalSeconds = (DateTime.Now - lastTimes[key]).TotalSeconds;
+
+                Console.ForegroundColor = totalSeconds switch
+                {
+                    > 30 => ConsoleColor.DarkGreen,
+                    > 10 => ConsoleColor.Green,
+                    > 5 => ConsoleColor.Yellow,
+                    _ => ConsoleColor.Red,
+
+                };
+
+                Console.Write($"{key}:{SplitTime(totalSeconds)} | ");
+            }
+
+        }
+
+        private string SplitTime(double timeSpan) =>
+            timeSpan switch
+            {
+                < 60 => Math.Round(timeSpan) + "s",
+                < 3600 => Math.Round(timeSpan / 60) + "+ m",
+                < 86400 => Math.Round(timeSpan / 3600) + "+ h",
+                _ => Math.Round(timeSpan / 86400) + "+ d"
+            };
+    }
+}
---
+++ PingConMonitor/PingConMonitor.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.NetworkInformation;
+using PingConMonitor.DataStructures;
+
+Console.WriteLine("Hello, Ping!\nv0.0");
+
+const string Address = "8.8.8.8";
+
+Ping ping = new();
+List<PingPoint> points = [];
+LastTimes lastTimes = new();
+Console.CursorVisible = false;
+
+while (!Console.KeyAvailable)
+{
+    PingReply reply = ping.Send(Address);
+    points.Add(new PingPoint(reply.RoundtripTime));
+    lastTimes.Update(reply.RoundtripTime);
+
+    ColorizeByReplyTime(reply);
+
+    Console.Write($"{reply.Status}: {reply.RoundtripTime}\t");
+    lastTimes.ShowAll();
+    Thread.Sleep(500);
+    Console.WriteLine();
+}
+
+static void ColorizeByReplyTime(PingReply reply)
+{
+    Console.ForegroundColor = reply.RoundtripTime switch
+    {
+        > 1500 => ConsoleColor.Red,
+        > 250 => ConsoleColor.Yellow,
+        > 100 => ConsoleColor.Green,
+        _ => ConsoleColor.DarkGreen,
+    };
+
+    Console.BackgroundColor = reply.Status switch
+    {
+        IPStatus.TimedOut => ConsoleColor.DarkGray,
+        _ => ConsoleColor.Black,
+    };
+}
